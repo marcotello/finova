c9f679f3c37dc66e79142cdcf77ddbb461d9500b
Marco Tello <marcotello5@outlook.com>
2026-04-09 00:13:38 -0600

    Creating the structure for the layouts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { Main } from './layout/main-layout/main/main';
+import { FullScreenLayout } from './layout/full-screen-layout/full-screen-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+    },
+    {
+        path: '',
+        component: Main,
+        children: [
+            {
+                path: 'dashboard',
+                loadComponent: () => import('./features/dashboard/pages/dashboard-page/dashboard-page').then(m => m.DashboardPage)
+            }
+        ]
+    },
+    {
+        path: 'auth',
+        component: FullScreenLayout,
+        children: [
+            {
+                path: 'login',
+                loadComponent: () => import('./features/auth/login/pages/login-page/login-page').then(m => m.LoginPage)
+            }
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: '/dashboard'
+    }
+];
--- src/app/layout/main-layout/main/main.ts
+++ src/app/layout/main-layout/main/main.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Sidebar } from "../sidebar/sidebar";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [Sidebar, RouterOutlet],
   templateUrl: './main.html',
   styleUrl: './main.css',
 })
